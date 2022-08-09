f5d7f3d873fc4598459fafb40838a317e27ef8a2
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-08-09 23:11:30 +0600

    Add  CheckBoxStyle

---
+++ Devote/Style/CheckBoxStyle.swift
@@ -0,0 +1,32 @@
+//
+//  CheckBoxStyle.swift
+//  Devote
+//
+//  Created by Shishir_Mac on 9/8/22.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack{
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .foregroundColor(configuration.isOn ? .pink : .primary)
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            
+            configuration.label
+        }// HStack
+    }
+}
+
+struct CheckBoxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder", isOn: .constant(true))
+            .padding()
+            .previewLayout(.sizeThatFits)
+            .toggleStyle(CheckBoxStyle())
+    }
+}
---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,30 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Shishir_Mac on 9/8/22.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        } // Toggle
+        .toggleStyle(CheckBoxStyle())
+        .onReceive(item.objectWillChange) { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        }
+    }
+}
